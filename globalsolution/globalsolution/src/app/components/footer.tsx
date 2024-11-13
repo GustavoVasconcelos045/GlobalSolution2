adf90e6cefc4e12b3fd673784c07cfa131a0ee3a
GustavoVasconcelos045 <gustavovasribeiro@hotmail.com>
2024-11-12 22:04:39 -0300

    cirando footer e header

---
+++ globalsolution/globalsolution/src/app/components/footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+    return (
+        <footer className="bg-white p-4 mt-8 text-center shadow-md h-[10vh]">
+            <p className="text-sm">&copy; 2024 GIF motors - Todos os direitos reservados.</p>
+        </footer>
+    );
+}
---
+++ globalsolution/globalsolution/src/app/components/header.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <header className="p-4 h-[20vh]">
+        <header className="p-4">
+            <h1 className="text-5xl font-bold mb-4">GIF Motors</h1>
+
+            function Navbar() {
+                    <nav>
+                        <Link href="/">Início</Link>
+                        <Link href="/monitoramento">Monitoramento</Link>
+                        <Link href="/educacao">Educação</Link>
+                        <Link href="/configuracoes">Configurações</Link>
+                    </nav>
+            
+                }
+        </header>   
+        </header>
+    );
+}
